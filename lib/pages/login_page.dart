1fb73453be321c89cdca334f30244ed9d5873456
DIBA <diba13dp@gmail.com>
2021-12-03 16:31:05 +0530

    day7 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,87 +11,101 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = '';
+  final _formKey = GlobalKey<FormState>();
+  moveTohome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/image/login.png',
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Welcome $name',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/image/login2.png',
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'User Id',
-                      hintText: 'Enter Your User Id',
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter Your Password',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    child: Container(
-                      width: 200,
-                      height: 50,
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome $name',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'User Id',
+                        hintText: 'Enter Your User Id',
                       ),
-                      decoration: BoxDecoration(
-                          color: Colors.orange[900],
-                          borderRadius: BorderRadius.circular(8)),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Please Enter Username";
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  //   },
-                  //   child: Text("Login"),
-                  //   style: TextButton.styleFrom(minimumSize: Size(200, 50)),
-                  // )
-                ],
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter Your Password',
+                      ),
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Enter the password";
+                        } else if (value != null && value.length < 4) {
+                          return "password shoud contain atleast 4 character";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InkWell(
+                      onTap: () => moveTohome(context),
+                      child: Container(
+                        width: 200,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.orange[900],
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
